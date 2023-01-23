d6310847ef47ca8755727f98980f83779d501715
Antoine Buard <a.buard@laposte.net>
2023-01-23 22:43:18 +0100

    Résolution du bug des imgs

--- js/index.js
+++ js/index.js
@@ -6,49 +6,48 @@ let activeImg = null;
 let activeImgParent = null;
 
 imgs.forEach((img) => {
-  let theImgMask = img.querySelector(".wrap");
+  let theImgWrap = img.querySelector(".wrap");
   let theImg = img.querySelector("img");
 
   img.addEventListener("click", () => {
-    const imgState = Flip.getState([theImgMask, theImg]);
+    const imgState = Flip.getState([theImgWrap, theImg]);
     gsap.set(imgPrev, { autoAlpha: 1 });
-    activeImg = theImgMask;
-    activeMaskImg = theImgMask.querySelector("img");
-    activeImgParent = theImgMask.parentNode;
-    imgPrev.appendChild(theImgMask);
+    activeImg = theImgWrap;
+    activeWrapImg = theImgWrap.querySelector("img");
+    activeImgParent = theImgWrap.parentNode;
+    imgPrev.appendChild(theImgWrap);
 
     Flip.from(imgState, {
       duration: 1,
       ease: "power3.inOut",
       scale: true,
       onStart: () => {
-        imgPrev.classList.add("imgPreview--active");
+        imgPrev.classList.toggle("imgPrev--active");
       },
       onComplete: () => {
-        isimgPreviewOpen = true;
+        isimgPrevOpen = true;
       },
     });
   });
+});
 
-  imgPrev.addEventListener("click", (e) => {
-    const activeImgState = Flip.getState([activeImg, activeMaskImg]);
+imgPrev.addEventListener("click", () => {
+  const activeImgState = Flip.getState([activeImg, activeWrapImg]);
+  activeImgParent.appendChild(activeImg);
 
-    activeImgParent.appendChild(activeImg);
-
-    Flip.from(activeImgState, {
-      duration: 1,
-      ease: "power3.inOut",
-      absolute: true,
-      scale: true,
-      zIndex: 2000,
-      nested: true,
-      onStart: () => {
-        imgPrev.classList.remove("imgPreview--active");
-      },
-      onComplete: () => {
-        isimgPrevOpen = false;
-        gsap.set(imgPrev, { autoAlpha: 0 });
-      },
-    });
+  Flip.from(activeImgState, {
+    duration: 1,
+    ease: "power3.inOut",
+    absolute: true,
+    scale: true,
+    zIndex: 2000,
+    nested: true,
+    onStart: () => {
+      imgPrev.classList.toggle("imgPrev--active");
+    },
+    onComplete: () => {
+      isimgPrevOpen = false;
+      gsap.set(imgPrev, { autoAlpha: 0 });
+    },
   });
 });
